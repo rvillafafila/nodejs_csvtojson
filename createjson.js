fafd0b183ecb086795510ac5d5cab9444114c5cd
Ricardo Villafafila <ricardo.villafafila.robles@gmail.com>
2017-12-16 01:24:35 +0100

    Delete unnecesary files

--- createjson.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const csvFileName = 'customer-data.xls'
-const jsonFileName = 'customar-data.json'
-
-var string = "{'key':'name'}"
-var obj = JSON.parse(string)
-console.log(obj.key)
